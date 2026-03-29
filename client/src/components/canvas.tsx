39eaad900b92afdba5d3fa0fb81bb76c7647924c
itsm16 <mrinalg2k1@gmail.com>
2026-03-29 11:10:39 +0530

    remove glitchy/ auto change in shape edges --seed

--- client/src/components/canvas.tsx
+++ client/src/components/canvas.tsx
@@ -7,8 +7,56 @@ function Canvas() {
   const [isDragging, setDragging] = useState(false);
   const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
   const [lastViewport, setLastViewport] = useState({ x: 0, y: 0, zoom: 1 });
+  const [data, setData] = useState([
+    {
+      id: 1,
+      shape: 'rectangle',
+      x: 10,
+      y: 10,
+      width: 200,
+      height: 200,
+      fill: '',
+    },
+    {
+      id: 2,
+      shape: 'circle',
+      x: 300,
+      y: 100,
+      width: 100,
+      height: 100,
+    },
+    {
+      id: 3,
+      shape: 'rectangle',
+      x: 10,
+      y: 300,
+      width: 200,
+      height: 200,
+      fill: 'lightblue',
+    },
+    {
+      id: 4,
+      shape: 'circle',
+      x: 300,
+      y: 300,
+      width: 100,
+      height: 100,
+      fill: 'lightblue',
+    },
+    
+    // {
+    //   id: 3,
+    //   shape: 'arrow',
+    //   x: 500,
+    //   y: 300,
+    //   width: 100,
+    //   height: 100,
+    // }
+  ]);
 
-  // dragstart - x, y , lastview port - x,y
+  // TODO: implement drawing container (an array storing drawing objects on canvas)
+  // TODO: once drawing container is implemented, create "back to drawing" button, that will reset viewport to initial state
+  // or take user to the last saved viewport/ or first drawing in the drawing container
 
   const draw = () => {
     const canvas = canvasRef.current;
@@ -16,18 +64,78 @@ function Canvas() {
     
     if (!canvas || !ctx) return;
     
-    // Clear previous drawings
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     ctx.save();
+
+    canvas.style.backgroundColor = '#121212';
+    ctx.strokeStyle = 'white';
     
     ctx.translate(viewport.x, viewport.y);
     ctx.scale(viewport.zoom, viewport.zoom);
     
     // there will be drawings here / loop through drawings
     // from local storage or database , held by useState / zustand
+    
+    // data.forEach((item) => {
+    //   if(item.shape === 'rectangle') {
+    //     ctx.strokeRect(item.x, item.y, item.width, item.height);
+    //   }
+    // });
+    // ctx.roundRect(230, 10, 200, 100, [10, 40, 0, 30]);
+
+    
+    // // Start a new path
+    // ctx.beginPath();
+    // ctx.roundRect(300, 50, 200, 100, 8);
+    // ctx.stroke();
+
     const rc = rough.canvas(canvas);
-    rc.rectangle(10, 10, 200, 200);
+
+    data.forEach((item) => {
+      let seed = 10;
+      let roughness = 1.5;
+      if (item.shape === 'rectangle') {
+        rc.rectangle(
+          item.x,
+          item.y,
+          item.width,
+          item.height,
+          {
+            stroke: 'white',
+            fill: item.fill ? item.fill : "",
+            roughness,
+            seed
+          }
+        );
+      }
+      if (item.shape === 'circle') {
+        rc.circle(
+          item.x,
+          item.y,
+          item.width,
+          {
+            fill: item.fill ? item.fill : "",
+            stroke: 'white',
+            roughness,
+            seed
+          }
+        );
+      }
+
+      
+      // if(item.shape === 'arrow') {
+      //   rc.line(
+      //     item.x,
+      //     item.y,
+      //     item.x + item.width,
+      //     item.y + item.height,
+      //     {
+      //       stroke: 'white',
+      //       seed
+      //     }
+      //   );
+      // }
+      
+    });
 
     ctx.restore();
   }
